254de430fd70a17cce5ef65936656a308a98cc14
madmaverickminion <ultron0603@gmail.com>
2023-11-20 11:20:58 +0530

    code for 24*24*24

--- objective.m
+++ objective.m
@@ -1,22 +1,38 @@
 function val = objective(x)
-pos_bits=3;
+% camera grid 4X4X4
+% mesh grid 250X250X250
 num_param=5;
 sz=length(x)/5;
-
-rows=32;
-cols=32;
+camera_rows=4;
+camera_cols=4;
+rows=250;
+cols=250;
 
 final_matrix=zeros(rows,rows,rows);
+for i = 1:sz
+    pos_x_val=x(num_param*(i-1)+1);
+    pos_y_val=x(num_param*(i-1)+2);
+    pos_z_val=x(num_param*(i-1)+3);
+    for j= i+1:sz
+        curr_pos_x=x(num_param*(j-1)+1);
+        curr_pos_y=x(num_param*(j-1)+2);
+        curr_pos_z=x(num_param*(j-1)+3);
+        if pos_x_val == curr_pos_x && pos_y_val == curr_pos_y && pos_z_val == curr_pos_z
+            val=1000000;
+            return;
+        end
+    end
 
+end
 for i = 1:sz
     pos_x_val=x(num_param*(i-1)+1);
     pos_y_val=x(num_param*(i-1)+2);
     pos_z_val=x(num_param*(i-1)+3);
     theta_val=x(num_param*(i-1)+4);% angle from x-axis
     angle_val=x(num_param*(i-1)+5);% angle from y_axis
-    pos_x_val=round(pos_x_val*((rows-1)/(2^pos_bits -1)));
-    pos_y_val=round(pos_y_val*((rows-1)/(2^pos_bits -1)));
-    pos_z_val=round(pos_z_val*((rows-1)/(2^pos_bits -1)));
+    pos_x_val=round(pos_x_val*((rows-1)/(camera_rows -1)));
+    pos_y_val=round(pos_y_val*((rows-1)/(camera_cols -1)));
+    pos_z_val=round(pos_z_val*((rows-1)/(camera_cols -1)));
     if (pos_x_val==0) || (pos_x_val==rows-1) || (pos_y_val==0) || (pos_y_val==cols-1) || (pos_z_val==rows-1)        
         % t=is_inside([pos_x_val,pos_y_val],angle_val,rows,cols);
         
